7bd2cf58386fbceb4e7ba7fc1d38a79a53ee0c91
papayapeter <t.babel@gmx.com>
2021-05-19 14:55:09 +0200

    made it work

--- scraper.py
+++ scraper.py
@@ -8,7 +8,7 @@ import argparse
 
 def save_img(input, image_url, index, directory):
     try:
-        filename = input + str(index) + '.jpg'
+        filename = f'{input}_{str(index).zfill(5)}.jpg'
         response = requests.get(image_url, stream=True)
         image_path = os.path.join(directory, filename)
         with open(image_path, 'wb') as file:
@@ -20,22 +20,23 @@ def save_img(input, image_url, index, directory):
 def find_urls(input, url, driver, directory):
     driver.get(url)
     for _ in range(500):
-        driver.execute_script("window.scrollBy(0,10000)")
+        driver.execute_script('window.scrollBy(0,10000)')
         try:
             driver.find_element_by_css_selector('.mye4qd').click()
         except:
             continue
-    for index, image_url in enumerate(driver.find_elements_by_css_selector(".rg_i.Q4LuWd")):
+        time.sleep(1)
+    for index, image_url in enumerate(driver.find_elements_by_css_selector('.rg_i.Q4LuWd')):
         try:
             image_url.click()
-            image = driver.find_element_by_css_selector(
-                "#Sva75c > div > div > div.pxAole > div.tvh9oe.BIB1wf > c-wiz > div > div.OUZ5W > div.zjoqD > div > div.v4dQwb > a > img"
+            time.sleep(1.5)
+            image = driver.find_element_by_xpath(
+                '/html/body/div[2]/c-wiz/div[4]/div[2]/div[3]/div/div/div[3]/div[2]/c-wiz/div/div[1]/div[1]/div/div[2]/a/img'
                 ).get_attribute("src")
             print(image)
             save_img(input, image, index, directory)
-            time.sleep(1.5)
         except:
-            return
+            pass
 
 
 def main():
@@ -58,7 +59,10 @@ def main():
     if not os.path.isdir(directory):
         os.makedirs(directory)
     url = f'https://www.google.com/search?q={str(search)}&source=lnms&tbm=isch&sa=X&ved=2ahUKEwie44_AnqLpAhUhBWMBHUFGD90Q_AUoAXoECBUQAw&biw=1920&bih=947'
+
     find_urls(search, url, driver, directory)
+
+    print('all possible images scraped.')
 
 
 if __name__ == "__main__":
